441bb42a6cc63de43e4b8c1bc4220eb4c769e686
Paulo Silva <psilva@pgahq.com>
2018-07-02 10:14:48 -0400

    Add code layout

---
+++ src/calculateTeamPoints.js
@@ -0,0 +1,8 @@
+// Given an array of players
+// returns a sorted array of teams with the total team points for active players.
+// The output is sorted in descending order by points
+// input: [{ team: 'green', name: 'Bob', points: 5, isActive: true }, ...]
+// output: [{ team: 'green', points: 40 }, ...]
+module.exports = (players) => {
+  return []
+}
