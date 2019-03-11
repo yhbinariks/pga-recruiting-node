2cf171e2b9aa7b2b9a800fce260141c4736a7afb
MNITD <bogdanleblank@ukr.net>
2019-03-11 16:08:07 +0200

    added function and tests

--- src/__tests__/calculateTeamPoints.test.js
+++ src/__tests__/calculateTeamPoints.test.js
@@ -1,2 +1,54 @@
+const assert = require('assert')
+const calculateTeamPoints = require('../calculateTeamPoints')
+const players = require('./__data__/players')
+
 describe('calculateTeamPoints', () => {
+  it('empty input, should return []', () => {
+    const players = []
+    const expected = []
+    const actual = calculateTeamPoints(players)
+    assert.equal(actual.length, expected.length)
+  })
+
+  it('innactive players, should return []', () => {
+    const players = [
+      {team: 1, name: 'player1', isActive: false, points: 12},
+      {team: 1, name: 'player2', isActive: false, points: 40}
+    ]
+    const expected = []
+    const actual = calculateTeamPoints(players)
+    assert.equal(actual.length, expected.length)
+  })
+
+  it('two teams, should return array of two items', () => {
+    const players = [
+      {team: 1, name: 'player1', isActive: true, points: 12},
+      {team: 1, name: 'player2', isActive: true, points: 12},
+      {team: 2, name: 'player3', isActive: true, points: 40},
+      {team: 2, name: 'player4', isActive: true, points: 40}
+    ]
+    const expected = [{team: 1, points: 24}, {team: 2, points: 80}]
+    const actual = calculateTeamPoints(players)
+    assert.equal(actual.length, expected.length)
+  })
+
+  it('two teams with different points number, should return array of two items in desc', () => {
+    const players = [
+      {team: 1, name: 'player1', isActive: true, points: 12},
+      {team: 2, name: 'player2', isActive: true, points: 40}
+    ]
+    const expected = [{team: 2, points: 40}, {team: 1, points: 12}]
+    const actual = calculateTeamPoints(players)
+    assert.deepEqual(actual, expected)
+  })
+
+  it('many teams and players, should return array of items in desc', () => {
+    const expected = [
+      {team: 'green', points: 40},
+      {team: 'red', points: 20},
+      {team: 'blue', points: 10}
+    ]
+    const actual = calculateTeamPoints(players)
+    assert.deepEqual(actual, expected)
+  })
 })
--- src/calculateTeamPoints.js
+++ src/calculateTeamPoints.js
@@ -4,5 +4,13 @@
 // input: [{ team: 'green', name: 'Bob', points: 5, isActive: true }, ...]
 // output: [{ team: 'green', points: 40 }, ...]
 module.exports = (players) => {
-  return []
+  const teams = {}
+
+  players
+    .filter(player => player.isActive)
+    .forEach(player => {
+      teams[player.team] = (teams[player.team] || 0) + player.points
+    })
+
+  return Object.keys(teams).map(team => ({ team, points: teams[team] })).sort((teamA, teamB) => teamB.points - teamA.points)
 }
